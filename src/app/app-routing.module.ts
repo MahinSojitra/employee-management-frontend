af48108628f18ac9a00e81ec03b3200118239149
MahinSojitra <mahin.sojitra@outlook.com>
2025-05-27 18:04:47 +0530

    Update app-routing.module.ts to change route guard from canActivateChild to canActivate for MainLayoutComponent, enhancing route protection and simplifying guard implementation.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
-    canActivateChild: [AuthGuard],
+    canActivate: [AuthGuard],
     children: [
       {
         path: '',
